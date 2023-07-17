248c4a01478902efe4ab370f3f12a6b2e77a5fcc
mirch <mirceateodor.oprea@gmail.com>
2023-07-17 23:32:07 +0300

    Add error enum and refactor conversions

---
+++ src/errors.rs
@@ -0,0 +1,32 @@
+use thiserror::Error;
+
+/// Errors that can occur when parsing a number into a power of two.
+#[derive(Debug, Error)]
+pub enum PowerOfTwoError {
+    #[error("{0} is not a valid power of two")]
+    InvalidPowerOfTwo(u128),
+    #[error(
+        "{0} is not a valid input for conversion. Provide an integer greater than or equal to 1."
+    )]
+    InvalidInputi8(i8),
+    #[error(
+        "{0} is not a valid input for conversion. Provide an integer greater than or equal to 1."
+    )]
+    InvalidInputi16(i16),
+    #[error(
+        "{0} is not a valid input for conversion. Provide an integer greater than or equal to 1."
+    )]
+    InvalidInputi32(i32),
+    #[error(
+        "{0} is not a valid input for conversion. Provide an integer greater than or equal to 1."
+    )]
+    InvalidInputi64(i64),
+    #[error(
+        "{0} is not a valid input for conversion. Provide an integer greater than or equal to 1."
+    )]
+    InvalidInputi128(i128),
+    #[error(
+        "{0} is not a valid input for conversion. Provide an integer greater than or equal to 1."
+    )]
+    InvalidInputisize(isize),
+}
---
+++ src/power_of_two.rs
@@ -0,0 +1,224 @@
+use crate::errors::PowerOfTwoError;
+
+/// A type that represents a power of two. It holds a u128 internally, but can be constructed from any integer type.
+/// # Examples
+/// ```
+/// use binexp::PowerOfTwo;
+/// assert!(PowerOfTwo::try_from(1u8).is_ok());
+/// assert!(PowerOfTwo::try_from(2u16).is_ok());
+/// assert!(PowerOfTwo::try_from(3u32).is_err());
+/// assert!(PowerOfTwo::try_from(4u64).is_ok());
+/// assert!(PowerOfTwo::try_from(5u128).is_err());
+/// assert!(PowerOfTwo::try_from(6usize).is_err());
+/// assert!(PowerOfTwo::try_from(7i8).is_err());
+/// assert!(PowerOfTwo::try_from(8i16).is_ok());
+/// assert!(PowerOfTwo::try_from(9i32).is_err());
+/// assert!(PowerOfTwo::try_from(10i64).is_err());
+/// assert!(PowerOfTwo::try_from(11i128).is_err());
+/// assert!(PowerOfTwo::try_from(12isize).is_err());
+/// ```
+pub struct PowerOfTwo(u128);
+
+impl PowerOfTwo {
+    /// Parses a number into a power of two.
+    /// # Examples
+    /// ```
+    /// use binexp::PowerOfTwo;
+    /// assert!(PowerOfTwo::parse(1).is_ok());
+    /// assert!(PowerOfTwo::parse(2).is_ok());
+    /// assert!(PowerOfTwo::parse(3).is_err());
+    /// ```
+    pub fn parse(number: u128) -> Result<Self, PowerOfTwoError> {
+        if number < 1 || number & (number - 1) != 0 {
+            return Err(PowerOfTwoError::InvalidPowerOfTwo(number));
+        }
+
+        Ok(Self(number))
+    }
+}
+
+impl std::fmt::Display for PowerOfTwo {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut number = self.0;
+        let mut power = 0;
+
+        while number > 1 {
+            number >>= 1;
+            power += 1;
+        }
+
+        write!(f, "2^{} ({})", power, self.0)
+    }
+}
+
+impl TryFrom<u8> for PowerOfTwo {
+    type Error = PowerOfTwoError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        Self::parse(value as u128)
+    }
+}
+
+impl TryFrom<u16> for PowerOfTwo {
+    type Error = PowerOfTwoError;
+
+    fn try_from(value: u16) -> Result<Self, Self::Error> {
+        Self::parse(value as u128)
+    }
+}
+
+impl TryFrom<u32> for PowerOfTwo {
+    type Error = PowerOfTwoError;
+
+    fn try_from(value: u32) -> Result<Self, Self::Error> {
+        Self::parse(value as u128)
+    }
+}
+
+impl TryFrom<u64> for PowerOfTwo {
+    type Error = PowerOfTwoError;
+
+    fn try_from(value: u64) -> Result<Self, Self::Error> {
+        Self::parse(value as u128)
+    }
+}
+
+impl TryFrom<u128> for PowerOfTwo {
+    type Error = PowerOfTwoError;
+
+    fn try_from(value: u128) -> Result<Self, Self::Error> {
+        Self::parse(value)
+    }
+}
+
+impl TryFrom<usize> for PowerOfTwo {
+    type Error = PowerOfTwoError;
+
+    fn try_from(value: usize) -> Result<Self, Self::Error> {
+        Self::parse(value as u128)
+    }
+}
+
+impl TryFrom<i8> for PowerOfTwo {
+    type Error = PowerOfTwoError;
+
+    fn try_from(value: i8) -> Result<Self, Self::Error> {
+        if value < 0 {
+            return Err(PowerOfTwoError::InvalidInputi8(value));
+        }
+
+        Self::parse(value as u128)
+    }
+}
+
+impl TryFrom<i16> for PowerOfTwo {
+    type Error = PowerOfTwoError;
+
+    fn try_from(value: i16) -> Result<Self, Self::Error> {
+        if value < 0 {
+            return Err(PowerOfTwoError::InvalidInputi16(value));
+        }
+
+        Self::parse(value as u128)
+    }
+}
+
+impl TryFrom<i32> for PowerOfTwo {
+    type Error = PowerOfTwoError;
+
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        if value < 0 {
+            return Err(PowerOfTwoError::InvalidInputi32(value));
+        }
+
+        Self::parse(value as u128)
+    }
+}
+
+impl TryFrom<i64> for PowerOfTwo {
+    type Error = PowerOfTwoError;
+
+    fn try_from(value: i64) -> Result<Self, Self::Error> {
+        if value < 0 {
+            return Err(PowerOfTwoError::InvalidInputi64(value));
+        }
+
+        Self::parse(value as u128)
+    }
+}
+
+impl TryFrom<i128> for PowerOfTwo {
+    type Error = PowerOfTwoError;
+
+    fn try_from(value: i128) -> Result<Self, Self::Error> {
+        if value < 0 {
+            return Err(PowerOfTwoError::InvalidInputi128(value));
+        }
+
+        Self::parse(value as u128)
+    }
+}
+
+impl TryFrom<isize> for PowerOfTwo {
+    type Error = PowerOfTwoError;
+
+    fn try_from(value: isize) -> Result<Self, Self::Error> {
+        if value < 0 {
+            return Err(PowerOfTwoError::InvalidInputisize(value));
+        }
+
+        Self::parse(value as u128)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn zero_is_not_a_power_of_two() {
+        assert!(crate::PowerOfTwo::parse(0).is_err());
+    }
+
+    #[test]
+    fn one_is_a_power_of_two() {
+        assert!(crate::PowerOfTwo::parse(1).is_ok());
+    }
+
+    #[test]
+    fn two_is_a_power_of_two() {
+        assert!(crate::PowerOfTwo::parse(2).is_ok());
+    }
+
+    #[test]
+    fn powers_of_two_are_parsed_correctly() {
+        for i in 0..128 {
+            let number = 1 << i;
+            assert!(crate::PowerOfTwo::parse(number).is_ok());
+        }
+    }
+
+    #[test]
+    fn non_powers_of_two_are_not_parsed() {
+        for i in 1..128 {
+            let number = (1 << i) + 1;
+            assert!(crate::PowerOfTwo::parse(number).is_err());
+        }
+    }
+
+    #[test]
+    fn integer_types_can_be_converted_to_power_of_two() {
+        assert!(crate::PowerOfTwo::try_from(1u8).is_ok());
+        assert!(crate::PowerOfTwo::try_from(1u16).is_ok());
+        assert!(crate::PowerOfTwo::try_from(1u32).is_ok());
+        assert!(crate::PowerOfTwo::try_from(1u64).is_ok());
+        assert!(crate::PowerOfTwo::try_from(1u128).is_ok());
+        assert!(crate::PowerOfTwo::try_from(1usize).is_ok());
+
+        assert!(crate::PowerOfTwo::try_from(1i8).is_ok());
+        assert!(crate::PowerOfTwo::try_from(1i16).is_ok());
+        assert!(crate::PowerOfTwo::try_from(1i32).is_ok());
+        assert!(crate::PowerOfTwo::try_from(1i64).is_ok());
+        assert!(crate::PowerOfTwo::try_from(1i128).is_ok());
+        assert!(crate::PowerOfTwo::try_from(1isize).is_ok());
+    }
+}
